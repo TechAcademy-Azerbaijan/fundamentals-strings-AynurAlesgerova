feba938d94160a6e6c4240d4c95f4a8594dca51a
AynurAlesgerova <105042078+AynurAlesgerova@users.noreply.github.com>
2022-06-28 11:57:54 +0400

    Update main.js

--- task-7/main.js
+++ task-7/main.js
@@ -3,7 +3,20 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+    let str = result.input
+
+    
+    let k1=1
+    for (let i = 0; i < str.length; i++) {
+      if (str[i]==="k") {
+        if(str[i]===str[i+1]){
+            k1=k1+1
+        }
+        
+      }
+    }
+    console.log(k1);
+      
+    
+ });
+ 
