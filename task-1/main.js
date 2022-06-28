d5c6cf3c6296bf94d14b11fd3d77f6718224f18d
AynurAlesgerova <105042078+AynurAlesgerova@users.noreply.github.com>
2022-06-28 10:34:07 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -3,7 +3,11 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+    let a = parseInt(result.input.split(',')[0])
+    // let array = result.input.split(',')[1].split(' ')
+
+    let b = parseInt(result.input.split(',')[1])
+    let c = parseInt(result.input.split(',')[2])
+
+    console.log(Math.min(a,Math.min(b,c)));
 });
