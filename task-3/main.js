5ce52353c653765a131ac9b74df629397fcc1989
AynurAlesgerova <105042078+AynurAlesgerova@users.noreply.github.com>
2022-06-28 11:10:41 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -3,7 +3,30 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+    let str = result.input
+
+   
+    let k=0
+    let p=0
+ 
+    for (let i = 0; i < str.length; i++) {
+        if (parseInt(str[i])==2) {
+            k=k+1
+        }
+        else if(parseInt(str[i])==5){
+            p=p+1
+        }   
+    }
+    
+    
+    if (k>p) {
+
+        console.log("2");
+    }
+    else{
+        console.log("5");
+    }
+
+
+
 });
