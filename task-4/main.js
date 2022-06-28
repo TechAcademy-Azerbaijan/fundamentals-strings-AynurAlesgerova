56e5865f59f56867b20c6c178d7e10be83b6c4e2
AynurAlesgerova <105042078+AynurAlesgerova@users.noreply.github.com>
2022-06-28 11:34:44 +0400

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -3,7 +3,19 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+    let str = result.input
+
+    let k=0
+
+    for (let i = 0; i < str.length; i++) {
+        
+        if(str[i]==="!" || str[i]==="."){
+            k=k+1
+        }
+        
+    }
+    console.log(k);
+ 
+ 
+
 });
