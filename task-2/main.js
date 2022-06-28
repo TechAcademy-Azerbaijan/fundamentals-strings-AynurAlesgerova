5cf7306202886441f41a9bf62a9e0ba2552404d1
AynurAlesgerova <105042078+AynurAlesgerova@users.noreply.github.com>
2022-06-28 10:47:30 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -3,7 +3,17 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+    let n = parseInt(result.input.split(',')[0])
+
+    let a=parseInt(n/100)
+    let b=parseInt(n%10)
+
+    if(a!==b){
+
+        console.log(Math.max(a,b));
+    }
+    
+    else if(a===b){
+        console.log("=");
+    }
 });
