70a96b51b82dbc2b252e625c8f5440b6ff3916fe
AynurAlesgerova <105042078+AynurAlesgerova@users.noreply.github.com>
2022-07-01 00:22:17 +0400

    Update main.js

--- task-6/main.js
+++ task-6/main.js
@@ -3,7 +3,16 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+    let str=result.input
+    let strArray=str.split(" ")
+    let count=0
+    
+    for (let i = 0; i < strArray.length; i++){
+        if(strArray[i]!= "")(
+            count++
+        )
+    }
+    
+    
+    console.log(count);
 });
